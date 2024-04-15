11a5e894cf742bc2d81645e026ff2b53e4bcc2d9
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2024-04-15 15:25:19 +0530

    0.0.0.3

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import '@coreui/coreui/dist/css/coreui.min.css'
-import { CCarousel,CCarouselItem,CImage } from '@coreui/react'
 import "./Carousel.css"
 
 export default function Carousel() {
