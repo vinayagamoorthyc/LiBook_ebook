cae0513ecbb929a504117b3a71cb3014d6aa75a3
vinayagamoorthyc <sanjaiiti2709@gmail.com>
2024-04-15 15:32:50 +0530

    0.0.0.3

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import '@coreui/coreui/dist/css/coreui.min.css'
 import "./Carousel.css"
+import introimg from "../images/intro.png"
 
 export default function Carousel() {
   return (
     <center>
-       
+       <div className='intro_flex'>
+            <div>
+                <img width={500} src={introimg} alt="" />
+            </div>
+            <div style={{width:"50%"}}>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque rem quod ex iusto optio repellat natus illo numquam, vel molestiae necessitatibus maxime magni, similique eius sit ipsum quaerat? Voluptate, incidunt.</p>
+            </div>
+       </div>
     </center>
   )
 }
